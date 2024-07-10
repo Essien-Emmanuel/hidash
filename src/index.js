82b65980fa90494c2209d16b26ef385a5d51291e
Essien Emmanuel <essienemma300@gmail.com>
2024-07-10 23:08:32 +0100

    added test for map function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 module.exports = {
   forEach(arr, fn) {
-    for (let i = 0; i < arr.length; i++) {
-      const value = arr[i];
-      fn(value, i);
+    // for (let i = 0; i < arr.length; i++) {
+    //   const value = arr[i];
+    //   fn(value, i);
+    // }
+    for (let index in arr) {
+      fn(arr[index], index);
     }
+  },
+
+  map(arr, fn) {
+    const newArr = [];
+    for (let value of arr) {
+      newArr.push(fn(value));
+    }
+    return newArr;
   },
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,19 @@
-const { forEach } = require("../src/index");
+const { forEach, map } = require("../src/index");
 
 let sum = 0;
 forEach([1, 2, 3], (value) => {
   sum += value;
 });
 
+const result = map([1, 2, 3], (value) => {
+  return value * value;
+});
+
 if (sum !== 6) throw new Error("Expected summing array to equal 6");
+
+if (result[0] !== 1)
+  throw new Error(`Expected to have 1, but found ${result[0]}`);
+if (result[1] !== 4)
+  throw new Error(`Expected to have 1, but found ${result[0]}`);
+if (result[2] !== 9)
+  throw new Error(`Expected to have 1, but found ${result[0]}`);
