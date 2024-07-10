6976faff1fc58db785ef6c2286462a4c3bde55b0
Essien Emmanuel <essienemma300@gmail.com>
2024-07-11 00:03:48 +0100

    integrated mocha for test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,27 +1,17 @@
 const assert = require("assert");
-const colors = require("colors");
 
 const { forEach, map } = require("../src/index");
 
-function test(desc, fn) {
-  console.log(`----${desc}`);
-  try {
-    fn();
-  } catch (error) {
-    console.log(colors.red(error.message));
-  }
-}
-
-test("The forEach function", () => {
+it("The forEach function", () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => {
     sum += value;
   });
 
-  assert.strictEqual(sum, 6, "Expected forEach to sum the array");
+  assert.strictEqual(sum, 7, "Expected forEach to sum the array");
 });
 
-test("The map function", () => {
+it("The map function", () => {
   const result = map([1, 2, 3], (value) => {
     return value * value;
   });
